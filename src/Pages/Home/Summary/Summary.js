d3c86ecf4ac94ad4366ac1aa02f8ceb70c25e6c5
Rafsan <turjz2827@gmail.com>
2021-10-20 23:00:54 +0600

    add app

--- src/Pages/Home/Summary/Summary.js
+++ src/Pages/Home/Summary/Summary.js
@@ -6,29 +6,31 @@ import "./summary.css"
 
 const Summary = () => {
     return (
-        <div className="container bg-img my-5">
+        <div className="container p-0 bg-img my-5">
+            <div className="bg-color m-0">
             <Row className=" m-0" >
-                            <Col md={3} className="p-0">
+                            <Col md={3} className="p-3">
                                 <p className="fs-1"><FontAwesomeIcon icon={faMale}></FontAwesomeIcon></p>
                                 <p>50+</p>
                                 <p>DOCTORS</p>
                             </Col>
-                            <Col md={3} sm={12} className="p-0">
+                            <Col md={3} sm={12} className="p-3">
                                 <p className="fs-1"><FontAwesomeIcon icon={faNotesMedical}></FontAwesomeIcon></p>
-                                <p>250+</p>
+                                <p>270+</p>
                                 <p>LAB TESTS</p>
                             </Col>
-                            <Col md={3} className="p-0">
+                            <Col md={3} className="p-3 text-color">
                                 <p className="fs-1"><FontAwesomeIcon icon={faUserInjured}></FontAwesomeIcon></p>
-                                <p>5,000,000 + </p>
-                                <p>WE HAVE 330 CORPORATE <br /> CUSTOMERS WITH 5M+ PATIENTS</p>
+                                <p>7,000,000 + </p>
+                                <p>WE HAVE 190 stuff <br /> 500000+ PATIENTS</p>
                             </Col>
-                            <Col md={3} className="p-0">
+                            <Col md={3} className="p-3">
                                 <p className="fs-1"><FontAwesomeIcon icon={faHospital}></FontAwesomeIcon></p>
                                 <p>1 of the first</p>
-                                <p>GOVERNMENT-APPROVED <br /> PRIVATE LABS TO OFFER COVID-19 TESTING</p>
+                                <p>GOVERNMENT-APPROVED</p>
                             </Col>
                         </Row>
+            </div>
         </div>
     );
 };
